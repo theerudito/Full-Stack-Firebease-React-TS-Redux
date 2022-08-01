66503e6d63f56fbee7f94179f7906c8fbcf89eb7
theerudito <erudito.tv@gmail.com>
2022-08-01 12:19:26 -0500

    v2

--- src/Components/HELPERS/API-FIREBASE.tsx
+++ src/Components/HELPERS/API-FIREBASE.tsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
-
+import { getAuth } from "@firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_FIREBASE,
@@ -14,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const dbFirebase = getFirestore(app);
+export const Auth = getAuth(app);
